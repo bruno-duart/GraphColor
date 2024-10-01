24145fe1a0a5ac885e2a2dec9e653ef47fe3c77b
Bruno Duarte <brunoduarteoliveira@hotmail.com>
2024-10-01 11:45:05 -0300

    Random Double generator implemented

--- include/utils.h
+++ include/utils.h
@@ -3,5 +3,6 @@
 
 int randint(int min, int max);
 int randint_diff(int min, int max, int avoid);
+double randdouble(double lower_bound, double upper_bound);
 
 #endif
--- lib/utils.cpp
+++ lib/utils.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib> // Para usar rand()
 #include <ctime>   // Para usar time()
 
-int randint(int min, int max) 
+int randint(int min, int max)
 {
     return (rand() % (max - min + 1)) + min;
 }
@@ -15,4 +15,13 @@ int randint_diff(int min, int max, int avoid)
         num = randint(min, max);
     while (num == avoid);
     return num;
+}
+
+double randdouble(double lower_bound, double upper_bound)
+{
+    std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+    std::default_random_engine re;
+    double a_random_double = unif(re);
+
+    return a_random_double;
 }

7ae0ec0c5ada0d6f36a96d40a776987cdc9af909
Bruno Duarte <duarte.bruno@outlook.com.br>
2024-10-04 22:00:39 -0300

    Fixing abc.sh

--- shell_scripts/abc.sh
+++ shell_scripts/abc.sh
@@ -14,14 +14,12 @@ tail -n +2 "$csv_file" | while IFS=';' read -r instance_name V E D xk k group; d
         else
             output_folder="random"
         fi
-        for method in "p" "g"; do
-            for bees in 50 100 200 300 400; do
-                for limit in 5 10 25 50; do
-                    for max_iter in 20 50 70 80; do
-                        for n_test in {1..30}; do
-                            echo ./run_abc "inst: $instance_name.col" "colors: $k" "bees: $bees" "limit: $limit" "max_iter: $max_iter" "output: results_abc/$output_folder/$instance_name.txt" "method: $method" "test: $n_test"
-                            ./run_abc instances/$instance_name.col $k $bees $limit $max_iter results_abc/$output_folder/$instance_name.txt $method
-                        done
+        for bees in 50 100 200 300 400; do
+            for limit in 5 10 25 50; do
+                for max_iter in 20 50 70 80; do
+                    for n_test in {1..30}; do
+                        echo ./run_abc "inst: $instance_name.col" "colors: $k" "bees: $bees" "limit: $limit" "max_iter: $max_iter" "output: results_abc/$output_folder/$instance_name.txt" "method: $method" "test: $n_test"
+                        ./run_abc instances/$instance_name.col $k $bees $limit $max_iter results_abc/$output_folder/$instance_name.txt $method
                     done
                 done
             done
